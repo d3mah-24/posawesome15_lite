8d0b3670160b0f54f33668e8edf2a9474dc9db47
abdopcnet <abdopcnet@gmail.com>
2025-10-23 01:41:12 +0300

    Fix: Scope Material Design Icons CSS to POS app only
    - Added class 'posapp-mdi-css' to the MDI CSS link element
    - Implemented on_page_leave cleanup to remove MDI CSS when leaving POS app
    - Prevents MDI CSS from affecting other ERPNext documents globally

--- posawesome/posawesome/page/posapp/posapp.js
+++ posawesome/posawesome/page/posapp/posapp.js
@@ -11,8 +11,14 @@ frappe.pages['posapp'].on_page_load = function (wrapper) {
 
 	$('div.navbar-fixed-top').find('.container').css('padding', '0');
 
-	$("head").append("<link rel='stylesheet' href='/assets/posawesome/css/materialdesignicons.css'>");
+	// Load Material Design Icons CSS only for POS app
+	$("head").append("<link rel='stylesheet' href='/assets/posawesome/css/materialdesignicons.css' class='posapp-mdi-css'>");
 	
 	// Fix shortcut.js offsetWidth error by hiding layout-main-section
 	$("head").append("<style>.layout-main-section { display: none !important; }</style>");
+};
+
+frappe.pages['posapp'].on_page_leave = function() {
+	// Remove Material Design Icons CSS when leaving POS app
+	$("head").find("link.posapp-mdi-css").remove();
 };
