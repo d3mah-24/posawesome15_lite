74efd29f23e3f9d8f6c44190c4913d0d343bea57
abdopcnet <abdopcnet@gmail.com>
2025-10-24 20:33:17 +0300

    🔧 Update POS Offer server script (pos_offer.py)

--- posawesome/posawesome/doctype/pos_offer/pos_offer.py
+++ posawesome/posawesome/doctype/pos_offer/pos_offer.py
@@ -22,6 +22,12 @@ class POSOffer(Document):
         if self.discount_type == "Discount Percentage" and not self.discount_percentage:
             frappe.throw("Discount Percentage is required when Discount Type is 'Discount Percentage'")
 
+        # Validate date range
+        if self.valid_from and self.valid_upto:
+            from frappe.utils import getdate
+            if getdate(self.valid_from) > getdate(self.valid_upto):
+                frappe.throw("Valid From date cannot be after Valid Upto date")
+
         # Validate offer_type specific fields
         if self.offer_type == "item_code" and not self.item_code:
             frappe.throw("Item Code is required when Offer Type is 'Item Code'")
@@ -37,6 +43,3 @@ class POSOffer(Document):
 
         if self.offer_type == "customer_group" and not self.customer_group:
             frappe.throw("Customer Group is required when Offer Type is 'Customer Group'")
-
-        if self.offer_type == "grand_total" and not self.grand_total:
-            frappe.throw("Grand Total is required when Offer Type is 'Grand Total'")
