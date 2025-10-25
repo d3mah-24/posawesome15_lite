594ec2935a4e349e8b0c4085f4e0d28d00c010c2
abdopcnet <abdopcnet@gmail.com>
2025-10-25 13:40:57 +0300

    🔧 Refactor UpdateCustomer.vue - separate script logic to external UpdateCustomer.js file

--- posawesome/public/js/posapp/components/pos/UpdateCustomer.vue
+++ posawesome/public/js/posapp/components/pos/UpdateCustomer.vue
@@ -12,82 +12,83 @@
           </div>
 
           <div class="modal-body">
-          <div class="field-group">
-            <label class="field-label">Customer Name *</label>
-            <input type="text" v-model="customer_name" class="custom-input" placeholder="Enter name" />
-          </div>
+            <div class="field-group">
+              <label class="field-label">Customer Name *</label>
+              <input type="text" v-model="customer_name" class="custom-input" placeholder="Enter name" />
+            </div>
 
-          <div class="field-row">
-            <div class="field-group half">
-              <label class="field-label">Tax ID</label>
-              <input type="text" v-model="tax_id" class="custom-input" placeholder="Tax ID" />
+            <div class="field-row">
+              <div class="field-group half">
+                <label class="field-label">Tax ID</label>
+                <input type="text" v-model="tax_id" class="custom-input" placeholder="Tax ID" />
+              </div>
+              <div class="field-group half">
+                <label class="field-label">Mobile</label>
+                <input type="text" v-model="mobile_no" class="custom-input" placeholder="Mobile" />
+              </div>
             </div>
-            <div class="field-group half">
-              <label class="field-label">Mobile</label>
-              <input type="text" v-model="mobile_no" class="custom-input" placeholder="Mobile" />
-            </div>
-          </div>
 
-          <div class="field-row">
-            <div class="field-group half">
-              <label class="field-label">Email</label>
-              <input type="email" v-model="email_id" class="custom-input" placeholder="Email" />
+            <div class="field-row">
+              <div class="field-group half">
+                <label class="field-label">Email</label>
+                <input type="email" v-model="email_id" class="custom-input" placeholder="Email" />
+              </div>
+              <div class="field-group half">
+                <label class="field-label">Gender</label>
+                <select v-model="gender" class="custom-select">
+                  <option value="">Select</option>
+                  <option v-for="g in genders" :key="g" :value="g">{{ g }}</option>
+                </select>
+              </div>
             </div>
-            <div class="field-group half">
-              <label class="field-label">Gender</label>
-              <select v-model="gender" class="custom-select">
-                <option value="">Select</option>
-                <option v-for="g in genders" :key="g" :value="g">{{ g }}</option>
-              </select>
-            </div>
-          </div>
 
-          <div class="field-row">
-            <div class="field-group half">
-              <label class="field-label">Referral Code</label>
-              <input type="text" v-model="referral_code" class="custom-input" placeholder="Code" />
-            </div>
-            <div class="field-group half">
-              <label class="field-label">Date of Birth</label>
-              <input type="text" v-model="birthday" readonly @click="birthday_menu = true" class="custom-input"
-                placeholder="DOB" />
-              <div v-if="birthday_menu" class="custom-modal-overlay" @click="birthday_menu = false">
-                <div class="custom-modal small-modal" @click.stop>
-                  <div class="date-picker-container">
-                    <input type="date" v-model="birthday" class="custom-date-input" :max="frappe.datetime.now_date()" @change="birthday_menu = false" />
+            <div class="field-row">
+              <div class="field-group half">
+                <label class="field-label">Referral Code</label>
+                <input type="text" v-model="referral_code" class="custom-input" placeholder="Code" />
+              </div>
+              <div class="field-group half">
+                <label class="field-label">Date of Birth</label>
+                <input type="text" v-model="birthday" readonly @click="birthday_menu = true" class="custom-input"
+                  placeholder="DOB" />
+                <div v-if="birthday_menu" class="custom-modal-overlay" @click="birthday_menu = false">
+                  <div class="custom-modal small-modal" @click.stop>
+                    <div class="date-picker-container">
+                      <input type="date" v-model="birthday" class="custom-date-input" :max="frappe.datetime.now_date()"
+                        @change="birthday_menu = false" />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div class="field-row">
-            <div class="field-group half">
-              <label class="field-label">Customer Group *</label>
-              <select v-model="group" class="custom-select" required>
-                <option value="">Select</option>
-                <option v-for="g in groups" :key="g" :value="g">{{ g }}</option>
-              </select>
+            <div class="field-row">
+              <div class="field-group half">
+                <label class="field-label">Customer Group *</label>
+                <select v-model="group" class="custom-select" required>
+                  <option value="">Select</option>
+                  <option v-for="g in groups" :key="g" :value="g">{{ g }}</option>
+                </select>
+              </div>
+              <div class="field-group half">
+                <label class="field-label">Territory *</label>
+                <select v-model="territory" class="custom-select" required>
+                  <option value="">Select</option>
+                  <option v-for="t in territorys" :key="t" :value="t">{{ t }}</option>
+                </select>
+              </div>
             </div>
-            <div class="field-group half">
-              <label class="field-label">Territory *</label>
-              <select v-model="territory" class="custom-select" required>
-                <option value="">Select</option>
-                <option v-for="t in territorys" :key="t" :value="t">{{ t }}</option>
-              </select>
-            </div>
-          </div>
 
-          <div class="field-row" v-if="loyalty_program || loyalty_points">
-            <div class="field-group half" v-if="loyalty_program">
-              <label class="field-label">Loyalty Program</label>
-              <input type="text" v-model="loyalty_program" readonly class="custom-input readonly" />
+            <div class="field-row" v-if="loyalty_program || loyalty_points">
+              <div class="field-group half" v-if="loyalty_program">
+                <label class="field-label">Loyalty Program</label>
+                <input type="text" v-model="loyalty_program" readonly class="custom-input readonly" />
+              </div>
+              <div class="field-group half" v-if="loyalty_points">
+                <label class="field-label">Points</label>
+                <input type="text" v-model="loyalty_points" readonly class="custom-input readonly" />
+              </div>
             </div>
-            <div class="field-group half" v-if="loyalty_points">
-              <label class="field-label">Points</label>
-              <input type="text" v-model="loyalty_points" readonly class="custom-input readonly" />
-            </div>
-          </div>
           </div>
         </div>
 
@@ -104,291 +105,7 @@
   </div>
 </template>
 
-<script>
-import { evntBus } from '../../bus';
-import { API_MAP } from "../../api_mapper.js";
-
-const EVENT_NAMES = {
-  SHOW_MESSAGE: 'show_mesage',
-  ADD_CUSTOMER_TO_LIST: 'add_customer_to_list',
-  SET_CUSTOMER: 'set_customer',
-  FETCH_CUSTOMER_DETAILS: 'fetch_customer_details',
-  
-  OPEN_UPDATE_CUSTOMER: 'open_update_customer',
-  REGISTER_POS_PROFILE: 'register_pos_profile',
-  PAYMENTS_REGISTER_POS_PROFILE: 'payments_register_pos_profile',
-};
-
-const VALIDATION_MESSAGES = {
-  CUSTOMER_NAME_REQUIRED: 'Customer name is required.',
-  CUSTOMER_GROUP_REQUIRED: 'Customer group name is required.',
-  TERRITORY_REQUIRED: 'Territory name is required.',
-};
-
-const SUCCESS_MESSAGES = {
-  CUSTOMER_CREATED: 'Customer created successfully.',
-  CUSTOMER_UPDATED: 'Customer data updated successfully.',
-};
-
-const ERROR_MESSAGES = {
-  FAILED_TO_CREATE: 'Failed to create customer.',
-  FAILED_TO_LOAD_GROUPS: 'Error loading customer groups',
-  FAILED_TO_LOAD_TERRITORIES: 'Error loading territories',
-  FAILED_TO_LOAD_GENDERS: 'Error loading genders',
-};
-
-const CUSTOMER_TYPE = {
-  INDIVIDUAL: 'Individual',
-};
-
-const DB_LIMITS = {
-  CUSTOMER_GROUPS: 1000,
-  TERRITORIES: 5000,
-  GENDERS: 10,
-};
-
-const DEFAULT_VALUES = {
-  CUSTOMER_GROUP: 'Individual',
-  TERRITORY: 'Rest Of The World',
-};
-
-export default {
-  name: 'UpdateCustomer',
-  data() {
-    return {
-      customerDialog: false,
-      pos_profile: null,
-      customer_id: '',
-      customer_name: '',
-      tax_id: '',
-      mobile_no: '',
-      email_id: '',
-      referral_code: '',
-      birthday: null,
-      birthday_menu: false,
-      customer_type: CUSTOMER_TYPE.INDIVIDUAL,
-      gender: '',
-      loyalty_points: null,
-      loyalty_program: null,
-      group: '',
-      groups: [],
-      territory: '',
-      territorys: [],
-      genders: [],
-    };
-  },
-  
-  methods: {
-    close_dialog() {
-      this.customerDialog = false;
-      this.clear_customer();
-    },
-    clear_customer() {
-      this.customer_name = '';
-      this.tax_id = '';
-      this.mobile_no = '';
-      this.email_id = '';
-      this.referral_code = '';
-      this.birthday = '';
-      this.group = frappe.defaults.get_user_default('Customer Group');
-      this.territory = frappe.defaults.get_user_default('Territory');
-      this.customer_id = '';
-      this.customer_type = CUSTOMER_TYPE.INDIVIDUAL;
-      this.gender = '';
-      this.loyalty_points = null;
-      this.loyalty_program = null;
-    },
-
-    getCustomerGroups() {
-      if (this.groups.length > 0) return;
-
-      frappe.db
-        .get_list('Customer Group', {
-          fields: ['name'],
-          filters: { is_group: 0 },
-          limit: DB_LIMITS.CUSTOMER_GROUPS,
-          order_by: 'name',
-        })
-        .then((data) => {
-          if (data.length > 0) {
-            this.groups = data.map((el) => el.name);
-          }
-        })
-        .catch((err) => {
-          this.showMessage(ERROR_MESSAGES.FAILED_TO_LOAD_GROUPS, 'error');
-        });
-    },
-
-    getCustomerTerritorys() {
-      if (this.territorys.length > 0) return;
-
-      frappe.db
-        .get_list('Territory', {
-          fields: ['name'],
-          filters: { is_group: 0 },
-          limit: DB_LIMITS.TERRITORIES,
-          order_by: 'name',
-        })
-        .then((data) => {
-          if (data.length > 0) {
-            this.territorys = data.map((el) => el.name);
-          }
-        })
-        .catch((err) => {
-          this.showMessage(ERROR_MESSAGES.FAILED_TO_LOAD_TERRITORIES, 'error');
-        });
-    },
-
-    getGenders() {
-      frappe.db
-        .get_list('Gender', {
-          fields: ['name'],
-          page_length: DB_LIMITS.GENDERS,
-        })
-        .then((data) => {
-          if (data.length > 0) {
-            this.genders = data.map((el) => el.name);
-          }
-        })
-        .catch((err) => {
-          this.showMessage(ERROR_MESSAGES.FAILED_TO_LOAD_GENDERS, 'error');
-        });
-    },
-
-    validateForm() {
-      if (!this.customer_name) {
-        this.showMessage(VALIDATION_MESSAGES.CUSTOMER_NAME_REQUIRED, 'error');
-        return false;
-      }
-      if (!this.group) {
-        this.showMessage(VALIDATION_MESSAGES.CUSTOMER_GROUP_REQUIRED, 'error');
-        return false;
-      }
-      if (!this.territory) {
-        this.showMessage(VALIDATION_MESSAGES.TERRITORY_REQUIRED, 'error');
-        return false;
-      }
-      return true;
-    },
-
-    submit_dialog() {
-      if (!this.validateForm()) {
-        return;
-      }
-
-      const args = {
-        customer_id: this.customer_id,
-        customer_name: this.customer_name,
-        company: this.pos_profile.company,
-        tax_id: this.tax_id,
-        mobile_no: this.mobile_no,
-        email_id: this.email_id,
-        referral_code: this.referral_code,
-        birthday: this.birthday,
-        customer_group: this.group,
-        territory: this.territory,
-        customer_type: this.customer_type,
-        gender: this.gender,
-        method: this.customer_id ? 'update' : 'create',
-        pos_profile_doc: JSON.stringify(this.pos_profile),
-      };
-
-      frappe.call({
-        method: this.customer_id ? API_MAP.CUSTOMER.UPDATE_CUSTOMER : API_MAP.CUSTOMER.POST_CUSTOMER,
-        args: args,
-        callback: (r) => {
-          if (!r.exc && r.message.name) {
-            this.handleCustomerSuccess(r.message.name, args);
-          } else {
-            this.handleCustomerError();
-          }
-        },
-      });
-
-      this.customerDialog = false;
-    },
-
-    handleCustomerSuccess(customerName, args) {
-      const isUpdate = !!this.customer_id;
-      const message = isUpdate 
-        ? SUCCESS_MESSAGES.CUSTOMER_UPDATED 
-        : SUCCESS_MESSAGES.CUSTOMER_CREATED;
-
-      args.name = customerName;
-
-      if (!isUpdate) {
-        evntBus.emit(EVENT_NAMES.ADD_CUSTOMER_TO_LIST, args);
-        evntBus.emit(EVENT_NAMES.SET_CUSTOMER, customerName);
-        evntBus.emit(EVENT_NAMES.FETCH_CUSTOMER_DETAILS);
-      }
-
-      this.close_dialog();
-    },
-
-    handleCustomerError() {
-      this.showMessage(ERROR_MESSAGES.FAILED_TO_CREATE, 'error');
-    },
-
-    showMessage(text, color) {
-      evntBus.emit(EVENT_NAMES.SHOW_MESSAGE, { text, color });
-    },
-
-    populateCustomerData(data) {
-      if (!data) return;
-
-      this.customer_name = data.customer_name;
-      this.customer_id = data.name;
-      this.tax_id = data.tax_id;
-      this.mobile_no = data.mobile_no;
-      this.email_id = data.email_id;
-      this.referral_code = data.referral_code;
-      this.birthday = data.birthday;
-      this.group = data.customer_group;
-      this.territory = data.territory;
-      this.loyalty_points = data.loyalty_points;
-      this.loyalty_program = data.loyalty_program;
-      this.gender = data.gender;
-    },
-
-    registerEventListeners() {
-      evntBus.on(EVENT_NAMES.OPEN_UPDATE_CUSTOMER, this.handleOpenUpdateCustomer);
-      evntBus.on(EVENT_NAMES.REGISTER_POS_PROFILE, this.handleRegisterPosProfile);
-      evntBus.on(EVENT_NAMES.PAYMENTS_REGISTER_POS_PROFILE, this.handlePaymentsRegisterPosProfile);
-    },
-
-    handleOpenUpdateCustomer(data) {
-      this.customerDialog = true;
-      this.populateCustomerData(data);
-
-      this.getCustomerGroups();
-      this.getCustomerTerritorys();
-      this.getGenders();
-    },
-
-    handleRegisterPosProfile(data) {
-      this.pos_profile = data.pos_profile;
-    },
-
-    handlePaymentsRegisterPosProfile(data) {
-      this.pos_profile = data.pos_profile;
-    },
-  },
-
-  created() {
-    this.registerEventListeners();
-    
-    this.group = frappe.defaults.get_user_default('Customer Group') || DEFAULT_VALUES.CUSTOMER_GROUP;
-    this.territory = frappe.defaults.get_user_default('Territory') || DEFAULT_VALUES.TERRITORY;
-  },
-
-  beforeDestroy() {
-    // Clean up all event listeners
-    evntBus.$off(EVENT_NAMES.OPEN_UPDATE_CUSTOMER, this.handleOpenUpdateCustomer);
-    evntBus.$off(EVENT_NAMES.REGISTER_POS_PROFILE, this.handleRegisterPosProfile);
-    evntBus.$off(EVENT_NAMES.PAYMENTS_REGISTER_POS_PROFILE, this.handlePaymentsRegisterPosProfile);
-  }
-};
-</script>
+<script src="./UpdateCustomer.js" />
 
 <style scoped>
 .customer-modal {
@@ -634,6 +351,7 @@ export default {
   from {
     opacity: 0;
   }
+
   to {
     opacity: 1;
   }
@@ -644,6 +362,7 @@ export default {
     transform: translateY(-20px) scale(0.95);
     opacity: 0;
   }
+
   to {
     transform: translateY(0) scale(1);
     opacity: 1;
@@ -656,7 +375,7 @@ export default {
     width: 95%;
     margin: 20px;
   }
-  
+
   .custom-modal.small-modal {
     width: 90%;
     max-width: 280px;
