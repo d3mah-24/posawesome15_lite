ce6b89c58e15314cc0a23d8800e2d1a4b8ad5e92
Ahmed Muhamed <64283607+d3mah-24@users.noreply.github.com>
2025-10-26 17:00:46 +0100

    Clean up setup.py formatting
    Removed unnecessary whitespace and comments in setup.py.

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup, find_packages
 
 
-
 # No external dependencies required - frappe and erpnext are managed by bench
 
 install_requires = []
-
-
 
 
 # get version from __version__ variable in posawesome/__init__.py
@@ -15,29 +12,14 @@ install_requires = []
 from posawesome import __version__ as version
 
 
-
 setup(
-
-
     name="posawesome",
-
     version=version,
-
-
     description="posawesome",
-
-
     author="future-support",
-
-
     author_email="abdopcnet@gmail.com",
-
     packages=find_packages(),
-
     zip_safe=False,
-
     include_package_data=True,
-
     install_requires=install_requires,
-
 )
