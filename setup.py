44261b940b044f051d8dfa658333763560c53aff
Ahmed Muhamed <64283607+d3mah-24@users.noreply.github.com>
2025-10-26 16:25:32 +0100

    Update setup.py with new author and description

--- setup.py
+++ setup.py
@@ -1,20 +1,43 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+
+
 # No external dependencies required - frappe and erpnext are managed by bench
+
 install_requires = []
 
-# get all values from hooks.py
-from posawesome.hooks import app_publisher, app_email, app_name, version
+
+
+
+# get version from __version__ variable in posawesome/__init__.py
+
+
+from posawesome import __version__ as version
+
+
 
 setup(
-    name=app_name,
+
+
+    name="posawesome",
+
     version=version,
-    description=app_name,
-    author=app_publisher,
-    author_email=app_email,
+
+
+    description="posawesome",
+
+
+    author="future-support",
+
+
+    author_email="abdopcnet@gmail.com",
+
     packages=find_packages(),
+
     zip_safe=False,
+
     include_package_data=True,
+
     install_requires=install_requires,
+
 )
